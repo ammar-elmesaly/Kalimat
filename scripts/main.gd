4ec04cb5578496f9dfbd354d365094c9097f5190
Ammar Elmesaly <amarmazn4@gmail.com>
2025-06-30 15:32:49 +0300

    Add some comments and refactor dictionary system

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var right = preload("res://resources/right_slot.png")
 @onready var selected = preload("res://resources/selected_slot.png")
 @onready var wrong = preload("res://resources/wrong_slot.png")
+
 @onready var volumeOff = preload("res://resources/volume-mute.svg")
 @onready var volumeUp = preload("res://resources/volume-up.svg")
 
@@ -28,18 +29,20 @@ var input_blocked : bool = false
 var muted : bool = false
 var paused : bool = false
 
-var dictionarySet : Dictionary
+var dictionaryArray = Array()
 
 func _ready() -> void:
-	dictionarySet = loadDictionary()
-	answer = randomWord(dictionarySet)
+	dictionaryArray = loadDictionary()
+	answer = randomWord(dictionaryArray)
 	statusArray.resize(ALPHABET_LETTER_NUMBER)  # This sets the array for keeping keyboard buttons status
-	statusArray.fill("empty")					# (Yellow, Green, Dark Gray)
+	# This basically initializes the status of the 30 letters to be empty (the letters you click to choose from)
+	# and these letters have 4 states: (Yellow, Green, Dark Gray, Empty)
+	statusArray.fill("empty")
 	get_node("game/words/rows").get_child(row).get_child(col).get_node("Slot").texture = selected
 	for i in range(30):
 		var key = get_node("game/keyboard/GridContainer/key_" + str(i))
 		key.pressed.connect(_on_key_pressed.bind(key.text))  # This sets the buttons for listenting and binds the text as an argument
-
+		
 func _on_key_pressed(letter : String) -> void:
 	if input_blocked:
 		return
@@ -58,34 +61,34 @@ func _on_check_pressed() -> void:
 	if input_blocked:
 		return
 	input_blocked = true
-	const alphabet : String = "ابتثجحخدذرزسشصضطظعغفقكلمنهويةء"
+	const ALPHABET : String = "ابتثجحخدذرزسشصضطظعغفقكلمنهويةء"
 	var answerArray = answer.split("")
 	var guessedWord : String = ''.join(guessedWordArray)
 	# if a new dictionary is found, add this condition to the if-condition below:
-	# or !dictionarySet.has(guessedWord)
+	# or !dictionaryArray.has(guessedWord)
 	if len(guessedWord) != 5:
 		input_blocked = false
 		return
-		
+
 	for i in range(WORD_LENGTH):
 		await get_tree().create_timer(0.5).timeout  # time delay between each iteration
-		# Set a new style box for each letter in the keyboard instead of them sharing the same 
+		# Set a new style box for each letter in the keyboard instead of them sharing the same
 		# Stylebox
 		var styleBox = StyleBoxTexture.new()
 		styleBox.texture_margin_left = 20  # set texture margin for new style box
 		styleBox.texture_margin_right = 20
-		
+
 		# the sprite of the letter slot (in the words grid)
 		var slotSprite = get_node("game/words/rows").get_child(row).get_child(i).get_node("Slot")
-		var alphabetIndex : int = alphabet.find(guessedWordArray[i])  # index of the letter in Arabic Alphabet
-		
+		var alphabetIndex : int = ALPHABET.find(guessedWordArray[i])  # index of the letter in Arabic Alphabet
+
 		var charIndexInAnswer : int = answerArray.find(guessedWordArray[i], 0)  # if letter found in word it stores its index in the answer word
 		# this condition is the core logic of the game
 		if guessedWordArray.slice(i).count(guessedWordArray[i]) > answerArray.count(guessedWordArray[i]) and i != charIndexInAnswer:
 			slotSprite.texture = wrong
 			setStatus(styleBox, alphabetIndex, "wrong")
 			if !muted: get_node("game/sfx/wrong").play()
-			
+
 		elif i == charIndexInAnswer:
 				slotSprite.texture = right
 				setStatus(styleBox, alphabetIndex, "right")
@@ -95,17 +98,17 @@ func _on_check_pressed() -> void:
 			slotSprite.texture = misplaced
 			setStatus(styleBox, alphabetIndex, "misplaced")
 			if !muted: get_node("game/sfx/wrong").play()
-			
+
 		# The keyboard key node
 		var keyboardKey = get_node("game/keyboard/GridContainer").get_child(alphabetIndex)
-		
+
 		keyboardKey.add_theme_stylebox_override("normal", styleBox)
 		keyboardKey.add_theme_stylebox_override("pressed", styleBox)
 		keyboardKey.add_theme_stylebox_override("hover", styleBox)
-		
+
 		while paused:  # this stops for loop while pausing
 			await get_tree().process_frame
-		
+
 	if ("".join(guessedWordArray) == answer):
 		get_node("win screen").visible = true
 		get_node("shader layers/win layer").visible = true
@@ -122,7 +125,7 @@ func _on_check_pressed() -> void:
 	slot.get_node("Slot").texture = selected
 	input_blocked = false
 	guessedWordArray = ["", "", "", "", ""]
-	
+
 func _on_erase_pressed() -> void:
 	if input_blocked:
 		return
@@ -142,39 +145,35 @@ func setStatus(styleBox : StyleBoxTexture, index : int, status : String) -> void
 		statusArray[index] = "misplaced"
 	elif status == "wrong" and statusArray[index] != "right" and statusArray[index] != "misplaced":
 		statusArray[index] = "wrong"
-	
+
 	styleBox.texture = statusDict[statusArray[index]]
 
-func loadDictionary() -> Dictionary:
+func loadDictionary() -> Array:
 	var file = FileAccess.open('res://resources/arabic_dict.txt', FileAccess.READ)
-	var dict : Dictionary = {}
+	var dict = Array()
+	
 	if file:
 		while !file.eof_reached():
 			var word = file.get_line()
-			dict[word] = null
+			dict.append(word)
 	return dict
 
-func randomWord(dict : Dictionary) -> String:
+func randomWord(dict : Array) -> String:
 	var dictSize : int = dict.size()
 	var randomIndex : int = randi() % dictSize
-	var i : int = 0
-	for word in dict:
-		if i == randomIndex:
-			return word
-		i += 1
-	return ""
+	return dict[randomIndex]
 
 
 func _on_mute_pressed() -> void:
 	var styleBox = StyleBoxTexture.new()
-	
+
 	if muted == false:
 		styleBox.texture = volumeOff
 		muted = true
 	else:
 		styleBox.texture = volumeUp
 		muted = false
-	
+
 	get_node("game/controls/mute").add_theme_stylebox_override("normal", styleBox)
 	get_node("game/controls/mute").add_theme_stylebox_override("pressed", styleBox)
 	get_node("game/controls/mute").add_theme_stylebox_override("hover", styleBox)
